3604f329b78f5b286fd190d1a8f357fca577e197
Torng030 <a0983629185@gmail.com>
2024-06-28 13:34:54 +0800

    Add files via upload

---
+++ week15-1/week15-1/week16/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace week16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label 書號Label;
+            System.Windows.Forms.Label 書名Label;
+            System.Windows.Forms.Label 單價Label;
+            System.Windows.Forms.Label 數量Label;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this._p11_1dbDataSet = new week16._p11_1dbDataSet();
+            this.bindingNavigator1 = new System.Windows.Forms.BindingNavigator(this.components);
+            this.書籍BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.書籍TableAdapter = new week16._p11_1dbDataSetTableAdapters.書籍TableAdapter();
+            this.tableAdapterManager = new week16._p11_1dbDataSetTableAdapters.TableAdapterManager();
+            this.書號TextBox = new System.Windows.Forms.TextBox();
+            this.書名TextBox = new System.Windows.Forms.TextBox();
+            this.單價TextBox = new System.Windows.Forms.TextBox();
+            this.數量TextBox = new System.Windows.Forms.TextBox();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            書號Label = new System.Windows.Forms.Label();
+            書名Label = new System.Windows.Forms.Label();
+            單價Label = new System.Windows.Forms.Label();
+            數量Label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._p11_1dbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).BeginInit();
+            this.bindingNavigator1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.書籍BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // 書號Label
+            // 
+            書號Label.AutoSize = true;
+            書號Label.Location = new System.Drawing.Point(28, 57);
+            書號Label.Name = "書號Label";
+            書號Label.Size = new System.Drawing.Size(32, 12);
+            書號Label.TabIndex = 1;
+            書號Label.Text = "書號:";
+            // 
+            // 書名Label
+            // 
+            書名Label.AutoSize = true;
+            書名Label.Location = new System.Drawing.Point(28, 85);
+            書名Label.Name = "書名Label";
+            書名Label.Size = new System.Drawing.Size(32, 12);
+            書名Label.TabIndex = 3;
+            書名Label.Text = "書名:";
+            // 
+            // 單價Label
+            // 
+            單價Label.AutoSize = true;
+            單價Label.Location = new System.Drawing.Point(28, 113);
+            單價Label.Name = "單價Label";
+            單價Label.Size = new System.Drawing.Size(32, 12);
+            單價Label.TabIndex = 5;
+            單價Label.Text = "單價:";
+            // 
+            // 數量Label
+            // 
+            數量Label.AutoSize = true;
+            數量Label.Location = new System.Drawing.Point(28, 141);
+            數量Label.Name = "數量Label";
+            數量Label.Size = new System.Drawing.Size(32, 12);
+            數量Label.TabIndex = 7;
+            數量Label.Text = "數量:";
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataSource = this._p11_1dbDataSet;
+            this.bindingSource1.Position = 0;
+            // 
+            // _p11_1dbDataSet
+            // 
+            this._p11_1dbDataSet.DataSetName = "_p11_1dbDataSet";
+            this._p11_1dbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bindingNavigator1
+            // 
+            this.bindingNavigator1.AddNewItem = null;
+            this.bindingNavigator1.BindingSource = this.書籍BindingSource;
+            this.bindingNavigator1.CountItem = this.bindingNavigatorCountItem;
+            this.bindingNavigator1.DeleteItem = null;
+            this.bindingNavigator1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.toolStripLabel1,
+            this.toolStripLabel2,
+            this.toolStripLabel3});
+            this.bindingNavigator1.Location = new System.Drawing.Point(0, 0);
+            this.bindingNavigator1.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.bindingNavigator1.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.bindingNavigator1.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.bindingNavigator1.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.bindingNavigator1.Name = "bindingNavigator1";
+            this.bindingNavigator1.PositionItem = this.bindingNavigatorPositionItem;
+            this.bindingNavigator1.Size = new System.Drawing.Size(395, 25);
+            this.bindingNavigator1.TabIndex = 0;
+            this.bindingNavigator1.Text = "bindingNavigator1";
+            // 
+            // 書籍BindingSource
+            // 
+            this.書籍BindingSource.DataMember = "書籍";
+            this.書籍BindingSource.DataSource = this._p11_1dbDataSet;
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(27, 22);
+            this.bindingNavigatorCountItem.Text = "/{0}";
+            this.bindingNavigatorCountItem.ToolTipText = "項目總數";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "移到最前面";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "移到上一個";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "位置";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9F);
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "目前的位置";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveNextItem.Text = "移到下一個";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveLastItem.Text = "移到最後面";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // 書籍TableAdapter
+            // 
+            this.書籍TableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.UpdateOrder = week16._p11_1dbDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.書籍TableAdapter = this.書籍TableAdapter;
+            // 
+            // 書號TextBox
+            // 
+            this.書號TextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.書籍BindingSource, "書號", true));
+            this.書號TextBox.Location = new System.Drawing.Point(66, 54);
+            this.書號TextBox.Name = "書號TextBox";
+            this.書號TextBox.Size = new System.Drawing.Size(100, 22);
+            this.書號TextBox.TabIndex = 2;
+            // 
+            // 書名TextBox
+            // 
+            this.書名TextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.書籍BindingSource, "書名", true));
+            this.書名TextBox.Location = new System.Drawing.Point(66, 82);
+            this.書名TextBox.Name = "書名TextBox";
+            this.書名TextBox.Size = new System.Drawing.Size(100, 22);
+            this.書名TextBox.TabIndex = 4;
+            // 
+            // 單價TextBox
+            // 
+            this.單價TextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.書籍BindingSource, "單價", true));
+            this.單價TextBox.Location = new System.Drawing.Point(66, 110);
+            this.單價TextBox.Name = "單價TextBox";
+            this.單價TextBox.Size = new System.Drawing.Size(100, 22);
+            this.單價TextBox.TabIndex = 6;
+            // 
+            // 數量TextBox
+            // 
+            this.數量TextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.書籍BindingSource, "數量", true));
+            this.數量TextBox.Location = new System.Drawing.Point(66, 138);
+            this.數量TextBox.Name = "數量TextBox";
+            this.數量TextBox.Size = new System.Drawing.Size(100, 22);
+            this.數量TextBox.TabIndex = 8;
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(31, 22);
+            this.toolStripLabel1.Text = "新增";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(31, 22);
+            this.toolStripLabel2.Text = "刪除";
+            this.toolStripLabel2.Click += new System.EventHandler(this.toolStripLabel2_Click);
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(31, 22);
+            this.toolStripLabel3.Text = "更新";
+            this.toolStripLabel3.Click += new System.EventHandler(this.toolStripLabel3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 257);
+            this.Controls.Add(書號Label);
+            this.Controls.Add(this.書號TextBox);
+            this.Controls.Add(書名Label);
+            this.Controls.Add(this.書名TextBox);
+            this.Controls.Add(單價Label);
+            this.Controls.Add(this.單價TextBox);
+            this.Controls.Add(數量Label);
+            this.Controls.Add(this.數量TextBox);
+            this.Controls.Add(this.bindingNavigator1);
+            this.Name = "Form1";
+            this.Text = "巡覽版";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._p11_1dbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).EndInit();
+            this.bindingNavigator1.ResumeLayout(false);
+            this.bindingNavigator1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.書籍BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private _p11_1dbDataSet _p11_1dbDataSet;
+        private System.Windows.Forms.BindingNavigator bindingNavigator1;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.BindingSource 書籍BindingSource;
+        private _p11_1dbDataSetTableAdapters.書籍TableAdapter 書籍TableAdapter;
+        private _p11_1dbDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.TextBox 書號TextBox;
+        private System.Windows.Forms.TextBox 書名TextBox;
+        private System.Windows.Forms.TextBox 單價TextBox;
+        private System.Windows.Forms.TextBox 數量TextBox;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+    }
+}
+
---
+++ week15-1/week15-1/week16/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 這行程式碼會將資料載入 '_p11_1dbDataSet.書籍' 資料表。您可以視需要進行移動或移除。
+            this.書籍TableAdapter.Fill(this._p11_1dbDataSet.書籍);
+
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                書籍BindingSource.AddNew();
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message);  
+            } 
+        }
+
+        private void toolStripLabel2_Click(object sender, EventArgs e)
+        {
+            書籍BindingSource.RemoveAt(書籍BindingSource.Position);
+        }
+
+        private void toolStripLabel3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                書籍BindingSource.EndEdit();
+                書籍TableAdapter.Update(_p11_1dbDataSet);
+                MessageBox.Show("資料更新成功");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
